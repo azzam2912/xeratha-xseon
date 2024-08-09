18ae7d22e0c0353b5ef4b2391ba4574bacb56539
azzam2912 <azzam.gamers12@gmail.com>
2024-08-09 10:12:27 +0700

    added basic  auth

---
+++ src/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66b575f4002bf1f09c16');
+
+export const account = new Account(client);
+export const databases = new Databases(client);
